18bc5013be0f3eb00e8dc35be25319b4674a7613
satanna <fortuna.me@ukr.net>
2017-04-24 11:15:39 +0300

    anim1

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,26 @@
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
-});
- $(document).ready(function(){
-      $('.parallax').parallax();
+    $('.parallax').parallax();
+
+
+    $('.q-left')
+        .addClass('a-hide')
+        .viewportChecker({
+        classToAdd: 'a-show magictime spaceInLeft',
+        classToRemove: 'a-hide',
+        offset: 200
     });
+
+    $('.q-right')
+        .addClass('a-hide')
+        .viewportChecker({
+            classToAdd: 'a-show magictime spaceInRight',
+            classToRemove: 'a-hide',
+            offset: 200
+        });
+
+    // $('.q-left').hover(function () {
+    //     $(this).addClass('magictime puffIn');
+    // });
+
+});
